40bc1af6f6f212f896081520ad850b5450fbd775
sharon <ksharon8256@gmail.com>
2022-09-05 18:21:33 +0530

    changed to euler maruyama

--- pf.cpp
+++ pf.cpp
@@ -18,10 +18,10 @@ int N{100}; // #agents
 int n{500}; // #time steps
 constexpr float W{1200.};
 constexpr float H{1200.};
-constexpr float dt = 0.01;
-constexpr float beta = 60;
-constexpr float PI = 3.1415926535;
-constexpr float rt = 70.; // threshold distance
+constexpr float dt{0.01};
+constexpr float beta{60};
+constexpr float PI{3.1415926535};
+constexpr float rt{70.}; // threshold distance
 float w2, h2;
 
 float unitrand();
@@ -116,10 +116,11 @@ float interactionforcemag(float r)
     {
         float U0 = 2650, U1 = 30, U2 = 2, U3 = 1;
         float A0 = 8, A1 = 2, A2 = 25, A3 = 26;
+        // A0 = 40; //temp
         float force = 0;
         force += U0 * r * exp(-(pow((r / A0), 2)));
-        force += U2 * exp(-r / A2);
-        force -= U3 * pow(r - A3, 2) * Hv(r - A3);
+        // force += U2 * exp(-r / A2);
+        // force -= U3 * pow(r - A3, 2) * Hv(r - A3);
         force += U1 * (r - A1) * Hv(r - A1);
         return -force;
     }
@@ -132,63 +133,67 @@ float interactionforcemag(float r)
 void setaccn(CELL &A, CELL &B)
 {
     VEC2 dp = B.p - A.p;
-    float r = sqrt(dp.x * dp.x + dp.y * dp.y);
+    float r = sqrt(dp.x * dp.x + dp.y * dp.y); // change to vec.
     float f = interactionforcemag(r);
     VEC2 F(f * (dp.x / r), f * (dp.y / r));
     A.a += F;
 }
+
+/*Loops through every cell and again through every cell  */
 void looploop(CELL M[])
 {
     // interaction
-    // for (int i = 0; i < N; i++)
-    // {
-    //     M[i].a = VEC2(0., 0.);
-    //     for (int j = 0; j < N; j++)
-    //     {
+    for (int i = 0; i < N; i++)
+    {
+        M[i].a = VEC2(0., 0.);
+        for (int j = 0; j < N; j++)
+        {
 
-    //         if (i != j)
-    //         {
-    //             setaccn(M[i], M[j]);
-    //         }
-    //     }
-    // }
+            if (i != j)
+            {
+                setaccn(M[i], M[j]);
+            }
+        }
+    }
     // viscek
-    // for (int i = 0; i < N; i++)
-    // {
-    //     VEC2 a;
-    //     int ninr = 0;
-    //     VEC2 dv;
-    //     for (int j = 0; j < N; j++)
-    //     {
+    for (int i = 0; i < N; i++)
+    {
+        VEC2 a;
+        int ninr = 0;
+        VEC2 dv;
+        for (int j = 0; j < N; j++)
+        {
 
-    //         VEC2 dp = M[j].p - M[i].p;
-    //         float r = sqrt(pow(dp.x, 2) + pow(dp.y, 2));
-    //         if (r < rt)
-    //         {
-    //             dv = M[j].v - M[i].v;
-    //             a += dv;
-    //             ninr += 1;
-    //         }
-    //     }
+            VEC2 dp = M[j].p - M[i].p;
+            float r = sqrt(pow(dp.x, 2) + pow(dp.y, 2));
+            if (r < rt)
+            {
+                dv = M[j].v - M[i].v;
+                a += dv;
+                ninr += 1;
+            }
+        }
 
-    //     M[i].a += a * (beta / ninr);
-    // }
+        M[i].a += a * (beta / ninr);
+    }
 
     // noise
     for (int i = 0; i < N; i++)
     {
-        M[i].a = VEC2(0., 0.);//temp REMOVE when done
+        // M[i].a = VEC2(0., 0.);//temp REMOVE when done
         float sig0 = 150.;
         float sig1 = 300.;
-        float rho0 = N / (W * H); // reference density
+        float rho0 = N / (W * H); // reference density (change to rho1)
         float rho = 0.;
         float sig = 0.;
-        // float tau = 1.39;
-        float tau = 0.01;
+        float tau = 1.39;
+        // float tau = 0.01;
 
         VEC2 U = VEC2(randf(0, 1), randf(0, 1));
         VEC2 xi(sqrt(-2 * log(U.x)) * cos(2 * PI * U.y),
                 sqrt(-2 * log(U.x)) * sin(2 * PI * U.y));
+        float std1 = 0.01;
+        xi = xi * std1;
         float theta = 0.;
         for (int j = 0; j < N; j++)
         {
@@ -200,14 +205,18 @@ void looploop(CELL M[])
                 rho += 1;
             }
         }
-        rho /= 2 * PI * rt * rt;
+        rho /= PI * rt * rt;
         sig = sig0 + (sig1 - sig0) * (1 - rho / rho0);
-        M[i].eta += (xi - M[i].eta) * (dt / tau);
-        M[i].eta = M[i].eta / M[i].eta.mag(); //normalize eta
+        // M[i].eta += (xi - M[i].eta) * (dt / tau);
+        // euler maruyama
+        M[i].eta = M[i].eta - (M[i].eta) * (dt / tau);
+        M[i].eta = M[i].eta + (xi) * (sqrt(dt) / tau);
 
-        theta = atan2(M[i].eta.y, M[i].eta.x);
-        M[i].a += VEC2(cos(theta), sin(theta)) * sig;
+        // M[i].eta = M[i].eta / M[i].eta.mag(); //normalize eta
 
+        // theta = atan2(M[i].eta.y, M[i].eta.x);
+        // M[i].a += VEC2(cos(theta), sin(theta)) * sig;
+        M[i].a += M[i].eta * sig;
     }
 
     for (int i = 0; i < N; i++)
